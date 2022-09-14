b0828cef62624ebb3539b93c9b13a138498e4012
reemrev21 <reemrev21@gmail.com>
2022-09-14 09:49:21 +0900

    remove useless file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
 import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       folloca Web
     </div>
   )
