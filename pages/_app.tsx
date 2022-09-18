3c57df4500e4adf8cd1ead90082cb1fa0b1c2b58
byungmin12 <kbm940526@naver.com>
2022-09-18 20:46:33 +0900

    [feat] header component
    style theme 설정 및 ThemeProvider통해 테마 전달

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../src/config/style/theme';
 import {useLocationStore} from "../src/states/zustandTestState";
 
 function MyApp({ Component, pageProps }: AppProps) {
     const {location} = useLocationStore()
   return (
-      <>
+      <ThemeProvider theme={theme}>
           {location}
         <Component {...pageProps} />
-      </>
+      </ThemeProvider>
   )
 }
 
