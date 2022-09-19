eec37fbbc9aac5014e70e147aa8e02c1778b7d43
byungmin12 <kbm940526@naver.com>
2022-09-19 13:57:07 +0900

    [feat] header component
    styled-component 사용을ㅇ ㅟ한 next.config 설정
    
    https://styled-components.com/docs/tooling#babel-plugin

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  compiler: {
+    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+    styledComponents:  {
+      // Enabled by default in development, disabled in production to reduce file size,
+      // setting this will override the default for all environments.
+      displayName: true,
+      ssr: true,
+      fileName: true,
+      cssProp: true,
+      pure: true,
+    },
+  },
 }
 
 module.exports = nextConfig
