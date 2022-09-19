11d6adc832c3c7e47f12d0c853be5a161b835013
byungmin12 <kbm940526@naver.com>
2022-09-19 14:00:37 +0900

    [feat] header component
    목데이터 데이터 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,9 @@
 import type {NextPage} from 'next'
-import Link from 'next/link'
 
 const Home: NextPage = () => {
     return (
         <div>
             Folloca Web
-            <ul>
-                <li>
-                    <Link href="/trend">
-                        <a>move to trend</a>
-                    </Link>
-                </li>
-                <li><Link href="/mypage">
-                    <a>move to mypage</a>
-                </Link>
-                </li>
-
-                <li><Link href="/search">
-                    <a>move to search</a>
-                </Link></li>
-
-
-                <li><Link href="/selection">
-                    <a>move to selection</a>
-                </Link></li>
-
-
-                <li>
-                    <Link href="/signin">
-                        <a>move to signin</a>
-                    </Link>
-                </li>
-
-
-                <li>
-                    <Link href="/signup">
-                        <a>move to signup</a>
-                    </Link>
-                </li>
-
-
-            </ul>
-
         </div>
     )
 }
