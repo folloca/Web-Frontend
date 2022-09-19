565334b6e5b692751880214cfe9f3dbbae6a3b6e
byungmin12 <kbm940526@naver.com>
2022-09-19 15:07:43 +0900

    [feat] header component
    svg사용을 위한 @svgr/webpack import

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   swcMinify: true,
   compiler: {
     // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
-    styledComponents:  {
+    styledComponents: {
       // Enabled by default in development, disabled in production to reduce file size,
       // setting this will override the default for all environments.
       displayName: true,
@@ -14,6 +14,15 @@ const nextConfig = {
       pure: true,
     },
   },
-}
+  webpack: (config) => {
+    // 아래를 추가합니다.
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
