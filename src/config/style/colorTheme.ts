e9b3a9bf72ce04158f11653e1f54c60e8734f199
byungmin12 <kbm940526@naver.com>
2022-09-21 17:28:03 +0900

    [feat] header component
    컬로 폰트 미디어 테마 분리

---
+++ src/config/style/colorTheme.ts
@@ -0,0 +1,25 @@
+export const color = {
+  neutral: {
+    50: "#F6F7F7",
+    100: "#D7D7D7",
+    200: "#C5C5C5",
+    300: "#A5A5A5",
+    400: "#828282",
+  },
+  primary: {
+    50: "#E6F2FF",
+    100: "#C0DFFF",
+    200: "#82BFFF",
+    300: "#439FFF",
+    400: "#047FFF",
+  },
+  secondary: {
+    GREEN: "#00C1A0",
+    RED: "#D8276C",
+  },
+  error: "#ff4943",
+  shades: {
+    WHITE: "#ffffff",
+    BLACK: "#000000",
+  },
+};
