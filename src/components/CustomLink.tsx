5c11f11d81d116b444aec06306cf9f3b9f85a0fd
byungmin12 <kbm940526@naver.com>
2022-09-21 17:29:07 +0900

    [feat] header component
    링크 내부에서 컴포넌트 클래스 변경

--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -1,1 +1,20 @@
-import Link from "next/link";import React, {ReactNode} from "react";interface ICustomLinkProps {    children: ReactNode    url: string    className?: string}const CustomLink = ({children,url,className}:ICustomLinkProps) => {    return (        <Link href={url} ><a className={className}>{children}</a></Link>    )}export default CustomLink;
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { ReactNode } from "react";
+
+interface ICustomLinkProps {
+  children: ReactNode;
+  url: string;
+  className?: string;
+}
+
+const CustomLink = ({ children, url, className }: ICustomLinkProps) => {
+  const router = useRouter();
+  return (
+    <Link href={url}>
+      <a className={router.pathname.startsWith(url) ? `${className} Selected` : className}>{children}</a>
+    </Link>
+  );
+};
+
+export default React.memo(CustomLink);
