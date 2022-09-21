82273d942359c72fdda7bf72eb7be260a11027a4
byungmin12 <kbm940526@naver.com>
2022-09-21 17:27:44 +0900

    [feat] header component
    폰트 설정

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link href="//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css" rel="stylesheet" type="text/css" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
