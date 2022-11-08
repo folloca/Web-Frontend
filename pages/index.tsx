fe6e5d52b5a7b8daa18ea8f7322737d4f52dfd58
byungmin12 <kbm940526@naver.com>
2022-11-08 22:04:47 +0900

    [feat]#13 card component
    불필요 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
-import type {NextPage} from 'next'
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-    return (
-        <div>
-            Folloca Web
-        </div>
-    )
-}
+  return <div></div>;
+};
 
-export default Home
+export default Home;
