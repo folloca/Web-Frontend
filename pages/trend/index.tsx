4fae606a738c56569b2df329d8a66c8c2d8e8357
byungmin12 <kbm940526@naver.com>
2022-12-05 22:32:25 +0900

    [feat]#13 card component
    배너 컨테이너 분리

--- pages/trend/index.tsx
+++ pages/trend/index.tsx
@@ -1,65 +1,18 @@
 import type { NextPage } from "next";
-import Card from "../../src/components/Card";
 import styled from "styled-components";
-
-const CardMockData = [
-  {
-    id: Math.random().toString(36).substring(2, 12),
-    title: "노티드 X 상상마당",
-    thumbnail: "/assets/CardInitailImage.png",
-    description: "가구와 와플, 의외의 만남을 성수에서 만나보아요",
-    tags: ["가구와와플조합", "데이트코스", "팝업"],
-  },
-  {
-    id: Math.random().toString(36).substring(2, 12),
-    title: "노티드 X 상상마당",
-    thumbnail: "/assets/CardInitailImage.png",
-    description: "가구와 와플, 의외의 만남을 성수에서 만나보아요",
-    tags: ["가구와와플조합", "데이트코스", "팝업"],
-  },
-  {
-    id: Math.random().toString(36).substring(2, 12),
-    title: "노티드 X 상상마당",
-    thumbnail: "/assets/CardInitailImage.png",
-    description: "가구와 와플, 의외의 만남을 성수에서 만나보아요",
-    tags: ["가구와와플조합", "데이트코스", "팝업"],
-  },
-  {
-    id: Math.random().toString(36).substring(2, 12),
-    title: "노티드 X 상상마당",
-    thumbnail: "/assets/CardInitailImage.png",
-    description: "가구와 와플, 의외의 만남을 성수에서 만나보아요",
-    tags: ["가구와와플조합", "데이트코스", "팝업"],
-  },
-  {
-    id: Math.random().toString(36).substring(2, 12),
-    title: "노티드 X 상상마당",
-    thumbnail: "/assets/CardInitailImage.png",
-    description: "가구와 와플, 의외의 만남을 성수에서 만나보아요",
-    tags: ["가구와와플조합", "데이트코스", "팝업"],
-  },
-];
+import React from "react";
+import CardContainer from "../../src/containers/CardContainer";
+import BannerContainer from "../../src/containers/BannerContainer";
 
 const Trend: NextPage = () => {
   return (
     <Wrapper>
       <section>
-        <div className={"banner"}>banner</div>
+        <BannerContainer />
       </section>
       <article>
         <section>
-          <CardContainer>
-            {CardMockData.map((data) => (
-              <Card
-                id={data.id}
-                title={data.title}
-                thumbnail={data.thumbnail}
-                description={data.description}
-                tags={data.tags}
-                key={data.id}
-              />
-            ))}
-          </CardContainer>
+          <CardContainer />
         </section>
         <section className={"popular"}>인기공간</section>
       </article>
@@ -83,10 +36,6 @@ const Wrapper = styled.main`
   `}
   }
 
-  .banner {
-    height: 320px;
-  }
-
   .popular {
     ${({ theme }) => theme.device.mobile`
         margin-bottom: 108px;
@@ -95,19 +44,6 @@ const Wrapper = styled.main`
 
   ${({ theme }) => theme.device.mobile`
         padding: 0 24px;
-  `}
-`;
-
-const CardContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-column-gap: 24px;
-  grid-row-gap: 48px;
-
-  ${({ theme }) => theme.device.desktop`
-        grid-template-columns: 1fr;
-        grid-column-gap: 0px;
-        grid-row-gap: 32px;
   `}
 `;
 
