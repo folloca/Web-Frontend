8b52a86ff737a2e78dc9c7b0d2ab858a04c8cc07
byungmin12 <kbm940526@naver.com>
2023-02-25 14:15:00 +0900

    [feat]#20 space 페이지
    CreateCard로 이름 변경 및 재사용성을 위해 수정

---
+++ src/components/CreateCard.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+import Plus from "../../public/assets/Plus.svg";
+
+interface ICreateCard {
+  text: string;
+}
+
+function CreateCard({ text }: ICreateCard) {
+  return (
+    <Wrapper>
+      <Plus />
+      <p>{text}</p>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  width: 417px;
+  min-height: 176px;
+  padding: 24px;
+  background: #fafafa;
+  border: 1px solid #047fff;
+  border-radius: 4px;
+  display: flex;
+
+  cursor: pointer;
+
+  & p {
+    font-family: "Spoqa Han Sans Neo";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 32px;
+    white-space: pre-wrap;
+  }
+`;
+
+export default CreateCard;
