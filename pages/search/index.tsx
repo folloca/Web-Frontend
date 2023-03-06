59eb56937d34352483f3da05efd9f0fa94cecfbd
reemrev21 <reemrev21@gmail.com>
2023-03-06 10:34:11 +0900

    [feature] #25 search bar 완료 - search bar - image width and height modify string to number

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,1 +1,21 @@
-import type { NextPage } from 'next'const Search: NextPage = () => {    return (        <div>            Search index page        </div>    )}export default Search
+import type { NextPage } from "next";
+import SearchBar from "../../src/components/SearchBar";
+import styled from "styled-components";
+
+const Search: NextPage = () => {
+  return (
+    <Wrapper>
+      <SearchBar />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 0 70px;
+`;
+
+export default Search;
