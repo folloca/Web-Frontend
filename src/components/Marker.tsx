eaf4c2699f6ff90a6be23ae042cd2b2c9c6647b0
byungmin12 <kbm940526@naver.com>
2023-03-20 23:26:06 +0900

    [feat]#23 post space
    도면에 마커 컴포넌트

---
+++ src/components/Marker.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+
+interface IMarker {
+  handlerDeleteMarker: (x: number, y: number) => void;
+  x: number;
+  y: number;
+  idx: number;
+}
+
+function Marker({ handlerDeleteMarker, x, y, idx }: IMarker) {
+  return (
+    <Wrapper
+      onClick={(e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        handlerDeleteMarker(x, y);
+      }}
+      top={y}
+      left={x}
+    >
+      {idx}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.span<{ top: number; left: number }>`
+  position: absolute;
+  transform: translate(-50%, -50%);
+  top: ${({ top }) => top}%;
+  left: ${({ left }) => left}%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #fafafa;
+  border: 1.5px solid #047fff;
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+`;
+
+export default Marker;
